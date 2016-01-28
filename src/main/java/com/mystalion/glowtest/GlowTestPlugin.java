968f10bcd6e64c8a7cd37050f7a9dca1f46f2074
Paulomart <paul@paul-h.de>
2016-01-28 17:25:29 +0100

    Initial commit

---
+++ src/main/java/com/mystalion/glowtest/GlowTestPlugin.java
@@ -0,0 +1,7 @@
+package com.mystalion.glowtest;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GlowTestPlugin extends JavaPlugin implements Listener {
+}
